ccdefe790f6d1b65800151b4628d210d6e35dc05
Karl Whitford <karl@einsteindbsql.com>
2020-08-25 04:31:06 -0700

    restore
    binpack buffers as buckets in a heap

---
+++ cloudPRAMS/torus/torustraits.go
@@ -0,0 +1,82 @@
+package torus
+
+// torusTraitsInterface encapsulates the properties of a torusInterface
+// that are required for other components of the system to interact with
+// it in a non-intrusive manner.
+type torusTraitsInterface interface {
+	// Capacity() specifies the overall torusCapacity of a torusInterface.
+	Capacity() torusCapacity
+	// DecayMultiplier specifies the decay multiplier that should take
+	// effect when a torusItem is placed on a particular torusInterface.
+	DecayMultiplier() float64
+	// TorusItemEvaluators provides access to the
+	// torusItemEvaluatorsInterface that defines the key behaviors (in
+	// evaluation contexts) of a torusItem when placed on a particular
+	// torusInterface.
+	TorusItemEvaluators() torusItemEvaluatorsInterface
+}
+
+// configurableTorusTraits is a concrete implementation of
+// torusTraitsInterface that allows easy customization of the properties
+// a torusTraitsInterface provides.
+type configurableTorusTraits struct {
+	capacity            torusCapacity
+	decayMultiplier     float64
+	torusItemEvaluators torusItemEvaluatorsInterface
+}
+
+func newConfigurableTorusTraits(capacity torusCapacity, decayMultiplier float64,
+	torusItemEvaluators torusItemEvaluatorsInterface) torusTraitsInterface {
+	return &configurableTorusTraits{
+		capacity:            capacity,
+		decayMultiplier:     decayMultiplier,
+		torusItemEvaluators: torusItemEvaluators,
+	}
+}
+
+func (cst *configurableTorusTraits) Capacity() torusCapacity {
+	return cst.capacity
+}
+
+func (cst *configurableTorusTraits) DecayMultiplier() float64 {
+	return cst.decayMultiplier
+}
+
+func (cst *configurableTorusTraits) TorusItemEvaluators() torusItemEvaluatorsInterface {
+	return cst.torusItemEvaluators
+}
+
+// torusTraitsFactoryInterface abstracts the creation of the
+// torusTraitsInterface for the 2 kinds of concrete torusInterface
+// implementations in our system.
+//
+// This allows for a pluggable hook in TorusManager, and is especially
+// useful for tests, but it is equally useful for running experiments
+// with different torusItemEvaluatorsInterface implementations (which
+// are indirectly accessible via
+// torusTraitsInterface.TorusItemEvaluators(), and which effectively
+// control the key evaluation behaviors of the system) in a
+// non-intrusive (and safe) manner, with minimal code changes.
+type torusTraitsFactoryInterface interface {
+	createSingleTemperatureTorusTraits(
+		torusItemEvaluators torusItemEvaluatorsInterface) torusTraitsInterface
+
+	createMultiTemperatureTorusTraits(
+		torusItemEvaluators torusItemEvaluatorsInterface) torusTraitsInterface
+}
+
+// productionTorusTraitsFactory is the real torusTraitsFactoryInterface
+// to use outside of a test environment.
+type productionTorusTraitsFactory struct{}
+
+func (pstf *productionTorusTraitsFactory) createSingleTemperatureTorusTraits(
+	torusItemEvaluators torusItemEvaluatorsInterface) torusTraitsInterface {
+	return newConfigurableTorusTraits(15, 1.0, torusItemEvaluators)
+}
+
+func (pstf *productionTorusTraitsFactory) createMultiTemperatureTorusTraits(
+	torusItemEvaluators torusItemEvaluatorsInterface) torusTraitsInterface {
+	return newConfigurableTorusTraits(20, 2.0, torusItemEvaluators)
+}
+
+var defaultTorusTraitsFactory = &productionTorusTraitsFactory{}
